d1bf40b7a224795135be32f33a6ad5acbcbaa055
lquasem <49497366+lquasem@users.noreply.github.com>
2019-08-18 23:12:17 -0700

    Mars Scraper

--- scrape_mars.py
+++ scrape_mars.py
@@ -20,11 +20,21 @@ def scrape():
     browser.visit(url)
     time.sleep(1)
     # scrape latest article title/para
+
+    
+    time.sleep(2)
+
+    # Scrape page into Soup
     html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
-    article_list = soup.find("ul", class_="item_list")
-    news_title = article_list.find("div", class_="content_title").text
-    news_p = article_list.find("div", class_="article_teaser_body").text
+    soup = BeautifulSoup(html, "html.parser")
+
+
+    title = soup.find("div", class_="content_title").get_text()
+    news_p = soup.find("div", class_="rollover_description_inner").get_text()
+
+
+
+
 
     # visit jpl's mars images site 
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
@@ -48,30 +58,60 @@ def scrape():
 
 
     # scrape table from mars facts
+       # Get Mars Facts
+    
     url = "https://space-facts.com/mars/"
+    browser.visit(url)
+    
+    time.sleep(2)
+    
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    
 
     mars_facts_df = pd.read_html(url)[0]
     mars_facts_df = mars_facts_df.rename(index=str, columns={0: "Description", 1: "Value"})
-    mars_facts_html = mars_facts_df.to_html(index='False')
+    mars_table = mars_facts_df.to_html(index='False')
 
-    #4 hemisphere URLs
+
+    
+    return mars_table
+
+     # hemisphere URLs
     hemis_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemis_url)
     time.sleep(2)
     
-    mars_hemis= [
-        {"title" : "Cerberus Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
-        {"title" : "Schiaparelli Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
-        {"title" : "Syrtis Major Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
-        {"title" : "Valles Marineris Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"}
-    ]
+    mars_hemis= []
+
+    for i in range (4):
+        time.sleep(5)
+        images = browser.find_by_tag('h3')
+        images[i].click()
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        partial = soup.find("img", class_="wide-image")["src"]
+        img_title = soup.find("h2",class_="title").text
+        img_url = 'https://astrogeology.usgs.gov'+ partial
+        dictionary={"title":img_title,"img_url":img_url}
+        mars_hemis.append(dictionary)
+        browser.back()
+
+   
+    #     {"title1" : "Cerberus Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg" },
+    #     {"title2" : "Schiaparelli Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
+    #     {"title3" : "Syrtis Major Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
+    #     {"title4" : "Valles Marineris Hemisphere", "img_url" : "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"}]
+
+    return mars_hemis
 
     mars_data = {
-        "news_title": news_title,
+        "news_title": title,
         "news_p": news_p,
         "featured_image_url": featured_image_url,
         "mars_weather": mars_weather,
-        "mars_facts": mars_facts_html,
+        "mars_facts": mars_table,
         "hemisphere_urls": mars_hemis
         
     }
